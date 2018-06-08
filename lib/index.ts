83a0abc750c5aa3eceb74a17f0f755fb5b4cd444
Ryan Tracey <ryan.m.tracey@gmail.com>
2018-06-07 21:33:59 -0400

    feat(barrel): add

---
+++ lib/index.ts
@@ -0,0 +1,17 @@
+import Candlestick from './candlestick';
+import CandlestickGroup from './candlestick';
+import * as constants from './constants';
+import Ohclv from './ohclv';
+import OhclvProcessor from './ohlcvProcessor';
+import Transaction from './transaction';
+
+const lib = {
+    Candlestick,
+    CandlestickGroup,
+    constants,
+    Ohclv,
+    OhclvProcessor,
+    Transaction,
+};
+
+export default lib;
