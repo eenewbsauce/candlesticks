1cd646fe07512851f5f04fc978248cc02ed6b372
eenewbsauce <ryan.m.tracey@gmail.com>
2018-06-08 14:06:44 -0400

    refactor(e2e): add real data fetch

--- e2e/index.ts
+++ e2e/index.ts
@@ -1,15 +1,16 @@
 import _ from 'lodash';
+import * as Quadriga from 'quadriga-api';
+import lib from '../lib';
+const { Transaction, OhclvProcessor } = lib;
+const quadriga = new Quadriga();
 
-import { txns } from './mockTransactions';
-import OhlcvProcessor  from '../lib/ohlcvProcessor';
-import Transaction from '../lib/transaction';
-
-const transactions = txns.map(txn => {
-    return new Transaction(txn);
+quadriga.transactions('eth_cad', 'hour', (err, txns) => {
+    const transactions = txns.map(txn => {
+        return new Transaction(txn);
+    });
+    
+    const ohlc = new OhclvProcessor(transactions, 10);
+    const ohlcs = ohlc.process();
+    
+    console.dir(ohlcs);
 });
-
-const ohlc = new OhlcvProcessor(transactions, 10);
-const ohlcs = ohlc.process();
-
-console.dir(ohlcs);
-
